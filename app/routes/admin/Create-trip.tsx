97275842db46fe9864c5213f32b1577dedb16ad4
builtbywilldev <builtbywill@proton.me>
2025-05-31 16:56:09 -0600

    🌍 Added dynamic map highlight for selected country - Integrated Syncfusion map components - Country selection now visually updates the map - First commit in map UI flow — continuing improvements next

--- app/routes/admin/Create-trip.tsx
+++ app/routes/admin/Create-trip.tsx
@@ -1,7 +1,11 @@
 import { Header } from "components";
-import React from "react";
+import React, { useState } from "react";
 import { ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
 import type { Route } from "./+types/Create-trip";
+import { comboBoxItems, selectItems } from "~/constants";
+import { formatKey } from "~/lib/utils";
+import { Coordinate, LayerDirective, LayersDirective, MapsComponent } from "@syncfusion/ej2-react-maps";
+import { world_map } from "~/constants/world_map";
 
 export const loader = async () => {
   const response = await fetch("https://restcountries.com/v3.1/all");
@@ -16,18 +20,39 @@ export const loader = async () => {
 };
 
 const Createtrip = ({ loaderData }: Route.ComponentProps) => {
+    const countries = loaderData as Country[];
+
+    const [formData, setFormData] = useState<TripFormData>({
+    country: countries[0]?.name || '', 
+    travelStyle: '',
+    interest: '',
+    budget: '',
+    duration: 0,
+    groupType: ''
+  })
+ 
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false)
+
+
   const handleSubmit = async () => {};
-  const handleChange = (key: keyof TripFormData, value: string | number) => 
-    {
-
-
-    }
-  const countries = loaderData as Country[];
+  const handleChange = (key: keyof TripFormData, value: string | number) => {
+    setFormData({ ... formData, [key]: value})
+  };
 
   const countryData = countries.map((country) => ({
     text: country.name,
     value: country.value,
   }));
+
+  const mapData= [
+    {
+        country: formData.country, 
+        color: '#EA382E',
+        coordinates: countries.find((c: Country) => c.name === formData.country)?.coordinates || []
+
+    }
+  ]
 
   return (
     <main className="flex flex-col gap-10 pb-20 wrapper">
@@ -45,25 +70,90 @@ const Createtrip = ({ loaderData }: Route.ComponentProps) => {
               fields={{ text: "text", value: "value" }}
               placeholder="Slect a country"
               className="combo-box"
-              change={(e: {value: string | undefined}) => {
-                if(e.value) {
-                    handleChange('country', e.value)
+              change={(e: { value: string | undefined }) => {
+                if (e.value) {
+                  handleChange("country", e.value);
                 }
-
               }}
-                allowFiltering
-                    filtering={(e) => {
-                        const query = e.text.toLowerCase();
-                        e.updateData(
-                            countries.filter((country) =>
-                            country.name.toLowerCase().includes(query)).map(((country) => ({
-                                text: country.name, 
-                                value: country.value
-                            })))
-                        )
-                    }}
+              allowFiltering
+              filtering={(e) => {
+                const query = e.text.toLowerCase();
+                e.updateData(
+                  countries
+                    .filter((country) =>
+                      country.name.toLowerCase().includes(query)
+                    )
+                    .map((country) => ({
+                      text: country.name,
+                      value: country.value,
+                    }))
+                );
+              }}
             />
           </div>
+          <div>
+            <label htmlFor="duration">Duration</label>
+            <input
+              id="duration"
+              name="duration"
+              type="number"
+              placeholder="Enter a number of days (5, 12...)"
+              className="form-input placeholder:text-gray-100"
+              onChange={(e) => handleChange("duration", Number(e.target.value))}
+            />
+          </div>
+          {selectItems.map((key) => (
+            <div key={key}>
+              <label htmlFor={key}>{formatKey(key)}</label>
+
+              <ComboBoxComponent
+                id={key}
+                dataSource={comboBoxItems[key].map((item) => ({
+                  text: item,
+                  value: item,
+                }))}
+                fields={{ text: "text", value: "value" }}
+                placeholder={`Select ${formatKey(key)}`}
+                change={(e: { value: string | undefined }) => {
+                  if (e.value) {
+                    handleChange(key, e.value);
+                  }
+                }}
+                allowFiltering
+                filtering={(e) => {
+                  const query = e.text.toLowerCase();
+                  e.updateData(
+                    comboBoxItems[key]
+                      .filter((item) => item.toLowerCase().includes(query))
+                      .map((item) => ({
+                        text: item,
+                        value: item,
+                      }))
+                  );
+                }}
+                className="combo-box"
+              />
+            </div>
+          ))}
+
+            <div>
+                <label htmlFor="location">
+                    Location on the world map
+                </label>
+                <MapsComponent>
+                    <LayersDirective>
+                        <LayerDirective 
+                            shapeData={world_map}
+                            dataSource={mapData}
+                            shapePropertyPath="name"
+                            shapeDataPath="country"
+                            shapeSettings={{ colorValuePath: 'color', fill: "#e5e5e5"}}
+                        />
+                    </LayersDirective>
+                </MapsComponent>
+            </div>
+          <div className="bg-gray-200 h-px w-full"/>
+
         </form>
       </section>
     </main>
